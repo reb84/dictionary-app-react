d4deda0199ac256174ab473c965ee32557e6008b
reb84 <152401972+reb84@users.noreply.github.com>
2024-05-31 21:48:43 +0100

    change api - basic state change with definition

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./DictionarySearch.css";
 
 export default function DictionarySearch() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function handleKeywordChange(event) {
@@ -16,8 +19,7 @@ export default function DictionarySearch() {
   function search(event) {
     event.preventDefault();
 
-    const apiKey = "22de0057ea42aa649cbcof0e3b1te784";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -36,6 +38,7 @@ export default function DictionarySearch() {
           </button>
         </div>
       </form>
+      <Results results={results} />
     </div>
   );
 }
